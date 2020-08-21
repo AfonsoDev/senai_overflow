21bcccda577207582b3ffd847f12679b46a06353
afonso <peresfon@gmail.com>
2020-08-20 22:22:23 -0300

    Web em desenvolvimento

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,31 +1,134 @@
-import React from "react";
-import {Container, ImageCropped, Form, Titulo, SubTitulo, InputGroup, Button} from "./styles"
-import foto from "../../assets/foto.jpg"
+import React, { useState } from "react";
+import {Container, ImageCropped, Form, Titulo, SubTitulo, InputGroup, Button} from "./styles";
+import foto from "../../assets/foto.jpg";
+import { api } from "../../services/api";
+
+const FormLogin = (props) =>{
+    
+    const [alunoLogin, setAlunoLogin] = useState({
+        email: "",
+        senha: ""
+    })
+    const handlerInput = (e) =>{
+        setAlunoLogin({...alunoLogin, [e.target.id]: e.target.value});
+        
+    }
+
+
+    const entrar = async (e) =>{
+
+        e.preventDefault();
+        try {
+            const retorno = await  api.post("/sessao", alunoLogin);
+            if(retorno.status == 201){
+                window.alert("Logado com sucesso");
+            }
+        } catch (erro) {
+          if(erro.response){
+           return window.alert(erro.response.data.erro);
+          }
+          window.alert("Ops.. Tente novamente mais tarde!");
+        }
+  
+    }
+    return(
+    <Form onSubmit={entrar}>
+        <Titulo>SENAI OVERFLOW</Titulo>
+        <SubTitulo>Compartilhe suas dúvidas</SubTitulo>
+        <InputGroup>
+            <label>E-mail</label>
+            <input type="email" value={alunoLogin.email} id="email" onChange={handlerInput} placeholder="Insira seu email" required></input>
+        </InputGroup>
+        <InputGroup>
+            <label>Senha</label>
+            <input type="password" value={alunoLogin.senha} id="senha" onChange={handlerInput} placeholder="Insira sua Senha" required></input>
+        </InputGroup>
+        <Button type="submit">
+            Entrar
+        </Button>
+        <Button type="button" onClick={() =>{
+            props.mostrarForm("registrar");
+        }}>
+            Registrar-se
+        </Button>
+    </Form>
+    )
+}
+const FormRegistrar = (props) =>{
+    
+    const [alunoRegistrar, setalunoRegistrar] = useState({
+        ra: "",
+        nome: "",
+        email: "",
+        senha: ""
+
+    })
+    const handlerInput = (e) =>{
+        setalunoRegistrar({...alunoRegistrar, [e.target.id]: e.target.value});
+        
+    }
+
+
+    const registrar = async (e) =>{
+
+        e.preventDefault();
+        try {
+            const retorno = await  api.post("/alunos", alunoRegistrar);
+            if(retorno.status == 201){
+                window.alert("Registrado com sucesso");
+            }
+        } catch (erro) {
+          if(erro.response){
+           return window.alert(erro.response.data.erro);
+          }
+          window.alert("Ops.. Tente novamente mais tarde!");
+        }
+  
+    }
+    return(
+    <Form onSubmit={registrar}>
+        <Titulo>SENAI OVERFLOW</Titulo>
+        <SubTitulo>Compartilhe suas dúvidas</SubTitulo>
+        <InputGroup>
+            <label>Ra</label>
+            <input type="number" value={alunoRegistrar.ra} id="ra" onChange={handlerInput} placeholder="Insira seu ra" required></input>
+        </InputGroup>
+        <InputGroup>
+            <label>Nome</label>
+            <input type="text" value={alunoRegistrar.nome} id="nome" onChange={handlerInput} placeholder="Insira seu nome" required></input>
+        </InputGroup>
+        <InputGroup>
+            <label>E-mail</label>
+            <input type="email" value={alunoRegistrar.email} id="email" onChange={handlerInput} placeholder="Insira seu email" required></input>
+        </InputGroup>
+        <InputGroup>
+            <label>Senha</label>
+            <input type="password" value={alunoRegistrar.senha} id="senha" onChange={handlerInput} placeholder="Insira sua Senha" required></input>
+        </InputGroup>
+        <Button type="submit">
+            Enviar
+        </Button>
+        <Button type="button" onClick={() =>{
+            props.mostrarForm("login");
+        }}>
+           Ja sou cadastrado
+        </Button>
+    </Form>
+    )
+}
 
 const Login = () =>{
+
+const [mostrarForm, setMostrarForm] = useState("login");
+   
     return(
         <Container>
             <ImageCropped>
                 <img src={foto} alt="Foto"/>
             </ImageCropped>
-            <Form>
-                <Titulo>SENAI OVERFLOW</Titulo>
-                <SubTitulo>Compartilhe suas dúvidas</SubTitulo>
-                <InputGroup>
-                    <label>E-mail</label>
-                    <input type="email" placeholder="Insira seu email"></input>
-                </InputGroup>
-                <InputGroup>
-                    <label>Senha</label>
-                    <input type="password" placeholder="Insira sua Senha"></input>
-                </InputGroup>
-                <Button>
-                    Entrar
-                </Button>
-                <Button>
-                    Registrar
-                </Button>
-            </Form>
+            {mostrarForm === "login" ?  <FormLogin mostrarForm={setMostrarForm}/>:<FormRegistrar mostrarForm={setMostrarForm} />}
+        
+         
         </Container>
     )
 }
--- web/src/pages/Login/styles.js
+++ web/src/pages/Login/styles.js
@@ -14,7 +14,8 @@ export const ImageCropped = styled.div`
     width: 100%;
     max-width: 580px;
     min-width: 100px;
-    height: 80%;
+    height: 90%;
+    min-height: 500px;
     max-height: 700px;
     overflow: hidden;
     border: 1px solid var(--white); 
@@ -28,8 +29,8 @@ export const ImageCropped = styled.div`
 export const Form = styled.form`
     width:100%;
     max-width: 400px;
-    height: 80%;
-
+    height: 90%;
+    min-height: 500px;
     border: 1px solid var(--white);
     border-radius: 0px 10px 10px 0px;
     border-left: 0px;
